3c2a740b973a2761466c64bba87320338664088a
john64012 <44446427+john64012@users.noreply.github.com>
2023-09-19 14:33:05 -0400

    Update random_encounters.py

---
+++ src/random_encounters.py
@@ -0,0 +1,21 @@
+import random
+
+min_value = 1
+max_value = 6
+roll_again = "yes"
+# while roll_again == "yes" or roll_again == "y":
+    print("Rolling the die...")
+    print("The value is....")
+      if value1 = 1,
+        then print(encounter1)
+    value1=random.randint(min_value, max_value)
+    print(value1)
+    # roll_again = input("Press 'y' or 'yes' to roll the die again.")
+
+#1 =
+  encounter1 = ""
+#2 =
+#3 = 
+#4 = 
+#5 = 
+#6 =
