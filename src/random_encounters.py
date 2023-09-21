6212a6a14f0ed7931ae1bea3b3998d652dc08458
john64012 <44446427+john64012@users.noreply.github.com>
2023-09-21 19:14:51 -0400

    Update random_encounters.py

--- src/random_encounters.py
+++ src/random_encounters.py
@@ -3,27 +3,26 @@ import random
 min_value = 1
 max_value = 6
 roll_again = "yes"
-# while roll_again == "yes" or roll_again == "y":
-    print("Rolling the die...")
-    print("The value is...." value1)
-      if value1 == 1,
-        then print(encounter1)
-      if value1 == 2,
-        then print(encounter2)
-      if value1 == 3,
-        then print(encounter3)
-      if value1 == 4,
-        then print(encounter4)
-      if value1 == 5,
-        then print(encounter5)
-      if value1 == 6,
-        then print(encounter6)
-    value1 = random.randint(min_value, max_value)
+value1 = random.randint(min_value, max_value)
+while roll_again == "yes" or roll_again == "y":
+    print("Rolling the die..."),
+    print("The value is....")
+    if value1 == 1:
+        print(encounter1)
+    if value1 == 2:
+        print(encounter2)
+    if value1 == 3:
+        print(encounter3)
+    if value1 == 4:
+        print(encounter4)
+    if value1 == 5:
+        print(encounter5)
+    if value1 == 6:
+        print(encounter6)
     print(value1)
     # roll_again = input("Press 'y' or 'yes' to roll the die again.")
 
 #1 =
-  encounter1 = ""
 #2 =
 #3 = 
 #4 = 
