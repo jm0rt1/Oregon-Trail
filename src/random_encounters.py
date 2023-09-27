d2b37b19398f4c22c38d6f5a505e4c33c11256a3
john64012 <44446427+john64012@users.noreply.github.com>
2023-09-27 15:06:04 -0400

    Update random_encounters.py
    Gave options to encounter 1

--- src/random_encounters.py
+++ src/random_encounters.py
@@ -22,7 +22,14 @@ while roll_again == "yes" or roll_again == "y":
     print(value1)
     # roll_again = input("Press 'y' or 'yes' to roll the die again.")
 
-encounter1 = "Your best ox has come down bad with tuberculosis. What do you do?"
+def encounter1:
+    "Your best ox has come down bad with tuberculosis. What do you do?"
+    "What do you want to do?"
+    "1. Kill the ox."
+    "2. Give the ox medicine."
+    "3. Use a local mix of natural herbs and spices in an attempt to cure the ox."
+    user_choice = input("Enter your choice(1, 2, or 3): ")
+
 #2 =
 #3 = 
 #4 = 
